422311cfcbf4a923fceafefae847e3d64b18c024
RahulSinghLeewayHertz <87765251+RahulSinghLeewayHertz@users.noreply.github.com>
2021-07-21 23:04:55 +0530

    Update JAVASp.js

--- JAVASp.js
+++ JAVASp.js
@@ -1,4 +1,4 @@
 document.write("hello word.......body");
 document.write("this is new branch");
 document.write("new change in this branch");
-document.write("same change in development and new-feature ");
+document.write("same change in development and new-feature");
